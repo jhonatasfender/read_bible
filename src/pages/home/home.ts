d95afeaa37dac58f6d4f9ffa529866ed4048e2b9
jhonatasfender <jhonatas.fender@gmail.com>
2018-07-02 23:08:54 -0300

    testando

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,7 +5,7 @@ import { db } from '../../db/db';
 
 import { lib } from '../../lib';
 
-declare var d3;
+declare var d3, $;
 
 @Component({
     selector: 'page-home',
@@ -13,11 +13,11 @@ declare var d3;
 })
 export class HomePage {
 
-    public print:any;
-    
+    public print: any;
+
     public lib: lib;
 
-    public db:any;
+    public db: any;
 
     public list: any;
 
@@ -27,34 +27,127 @@ export class HomePage {
         this.lib = new lib();
         // this.print = this.lib.syntaxHighlight(JSON.stringify(db, undefined, 4));
         this.db = new Array();
-        for(let i in db.json) {
-            this.db.push({
-                abbrev: db.json[i].abbrev,
-                color: this.lib.generatesColor()
-            });
+        for (let i in db.json) {
+            if(Array.isArray(db.json[i].chapters)) { 
+                this.db.push({
+                    text: db.json[i].abbrev,
+                    // abbrev: db.json[i].abbrev,
+                    // color: this.lib.generatesColor(),
+                    count: db.json[i].chapters.length
+                });
+            }
         }
+        console.log("teste")
     }
 
-    ngOnInit() {
-        var width = 960,
-        height = 500,
-        radius = 30;
-
-        var p0 = [250, 200, 60],
-        p1 = [560, 300, 120];
-
-        var svg = d3.select(".conteiner").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g");
-
-		svg.append("path")
-		.attr("class", "mesh")
-		// .attr("d", d3.hexbin().size([width, height]).radius(radius).mesh);
-		.attr("d", () => {
-			return "M 130,50 100,0 30,0 0,50 30,100 100,100 130,50"
-		});
-
+    ngAfterViewInit() {
+        var bubbleChart = new d3.svg.BubbleChart({
+            supportResponsive: true,
+            //container: => use @default
+            size: 600,
+            //viewBoxSize: => use @default
+            innerRadius: 600 / 3.5,
+            //outerRadius: => use @default
+            radiusMin: 50,
+            //radiusMax: use @default
+            //intersectDelta: use @default
+            //intersectInc: use @default
+            //circleColor: use @default
+            data: {
+                items: this.db,
+                eval: function(item) {
+                    console.log(item)
+                    return item.count;
+                },
+                classed: function(item) {
+                    return item.text.split(" ").join("");
+                }
+            },
+            /*plugins: [{
+                    name: "central-click",
+                    options: {
+                        text: "(See more detail)",
+                        style: {
+                            "font-size": "12px",
+                            "font-style": "italic",
+                            "font-family": "Source Sans Pro, sans-serif",
+                            //"font-weight": "700",
+                            "text-anchor": "middle",
+                            "fill": "white"
+                        },
+                        attr: {
+                            dy: "65px"
+                        },
+                        centralClick: function() {
+                            alert("Here is more details!!");
+                        }
+                    }
+                },
+                {
+                    name: "lines",
+                    options: {
+                        format: [{ // Line #0
+                                textField: "count",
+                                classed: {
+                                    count: true
+                                },
+                                style: {
+                                    "font-size": "28px",
+                                    "font-family": "Source Sans Pro, sans-serif",
+                                    "text-anchor": "middle",
+                                    fill: "white"
+                                },
+                                attr: {
+                                    dy: "0px",
+                                    x: function(d) {
+                                        return d.cx;
+                                    },
+                                    y: function(d) {
+                                        return d.cy;
+                                    }
+                                }
+                            },
+                            { // Line #1
+                                textField: "text",
+                                classed: {
+                                    text: true
+                                },
+                                style: {
+                                    "font-size": "14px",
+                                    "font-family": "Source Sans Pro, sans-serif",
+                                    "text-anchor": "middle",
+                                    fill: "white"
+                                },
+                                attr: {
+                                    dy: "20px",
+                                    x: function(d) {
+                                        return d.cx;
+                                    },
+                                    y: function(d) {
+                                        return d.cy;
+                                    }
+                                }
+                            }
+                        ],
+                        centralFormat: [{ // Line #0
+                                style: {
+                                    "font-size": "50px"
+                                },
+                                attr: {}
+                            },
+                            { // Line #1
+                                style: {
+                                    "font-size": "30px"
+                                },
+                                attr: {
+                                    dy: "40px"
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]*/
+        });
     }
 
 }
